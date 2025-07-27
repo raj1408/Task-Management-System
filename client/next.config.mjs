3c266177b5c1b6414e0038c4e8527c73ba7bf064
Raj Saini <101448363+raj1408@users.noreply.github.com>
2025-07-27 21:08:41 +0530

    Added readme

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://task-manager-seven-gamma.vercel.app/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
